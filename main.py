1dbaea6434df15ea300f490e3f8b22f84e94a635
kubijaku <jakubkubicki@onet.pl>
2022-10-15 10:48:22 +0200

    ni calculating

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 
 pi = 3.14
 g = 9.80
-def print_hi(masa, srednica, czas_sredni, objetosc):
+def ni_calculator(masa, srednica, czas_sredni, objetosc):
     D = 0.039
     l = 0.69
     t = 22
@@ -14,8 +14,9 @@ def print_hi(masa, srednica, czas_sredni, objetosc):
 
     all_ni = []
     for i in range(6):
-        wynik = ( (masa[i] - (pi * ro * math.pow(srednica[i],3) )/6 ) * g * czas_sredni[i] ) / ( 3 * pi * l * srednica[i] ( 1 + (2,4 * srednica[i]/D)))
-
+        wynik = ( (masa[i] - (pi * ro * math.pow(srednica[i],3) )/6 ) * g * czas_sredni[i] ) / ( 3 * pi * l * srednica[i] * ( 1 + (srednica[i] / D * 2.4 ) ) )
+        all_ni.append(wynik)
+    print(all_ni)
 
 
 
@@ -35,6 +36,7 @@ if __name__ == '__main__':
     masa = [ 0.000268, 0.000132, 0.000114, 0.000268, 0.000269, 0.000114]
     czas_sr = [4.98, 7.41, 7.76, 4.89, 4.92, 7.73]
     objetosc = obj(srednica)
+    ni_calculator(masa,srednica,czas_sr,objetosc)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
